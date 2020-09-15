ac1761efd0c70ccfd0294272326099dfe6982640
Nv7 <junk4Nv7@gmail.com>
2020-09-14 18:19:38 -0700

    setup vugu project, made wordsearch "under construction"

--- devserver.go
+++ devserver.go
@@ -17,7 +17,11 @@ func main() {
 	mux := devutil.NewMux()
 	mux.Match(devutil.NoFileExt, devutil.DefaultAutoReloadIndex.Replace(
 		`<!-- styles -->`,
-		`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">`))
+		`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	  <script src="https://cdn.jsdelivr.net/npm/text-encoding@0.7.0/lib/encoding.min.js"></script>`))
 	mux.Exact("/main.wasm", devutil.NewMainWasmHandler(wc))
 	mux.Exact("/wasm_exec.js", devutil.NewWasmExecJSHandler(wc))
 	mux.Default(devutil.NewFileServer().SetDir("."))
--- go.mod
+++ go.mod
@@ -1,11 +1,8 @@
-module github.com/vugu-examples/simple
+module github.com/Nv7-Github/nv7-github.github.io
 
 go 1.15
 
 require (
-	github.com/gordonklaus/ineffassign v0.0.0-20200809085317-e36bfde3bb78 // indirect
 	github.com/vugu/vjson v0.0.0-20200505061711-f9cbed27d3d9
-	github.com/vugu/vugu v0.3.2
-	golang.org/x/tools v0.0.0-20200913032122-97363e29fc9b // indirect
-	honnef.co/go/tools v0.0.1-2020.1.5 // indirect
+	github.com/vugu/vugu v0.3.3
 )
