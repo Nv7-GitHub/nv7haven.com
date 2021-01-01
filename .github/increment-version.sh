6fd2f33f4f1472efa94255d6eeb7cb9872e69e1f
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2020-12-31 17:05:26 -0800

    Update increment-version.sh

--- .github/increment-version.sh
+++ .github/increment-version.sh
@@ -1,4 +1,10 @@
+go generate
+echo "Generated"
+GOOS=js GOARCH=wasm go build -ldflags="-w -s" -o docs/main.wasm
+echo "Built"
+du -h docs/main.wasm
+
 THING=$(cat docs/index.html | pcregrep -o1 '\?v=([0-9]+)')
 THING=$((THING + 1))
-export THING
 sed -i '' "s/\?v=1/?v=${THING}/g" docs/index.html
+echo "Incremented Version to ${THING}"
