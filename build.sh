26277cad720c3412126578bd05f9c2ebcdc6d3d7
Nv7 <junk4Nv7@gmail.com>
2020-12-31 17:28:17 -0800

    New buildscript

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ echo "Generated"
 GOOS=js GOARCH=wasm go build -ldflags="-w -s" -o docs/main.wasm
 echo "Built"
 du -h docs/main.wasm
+
+THING=$(cat docs/index.html | pcregrep -o1 '\?v=([0-9]+)')
+THING=$((THING + 1))
+sed -i '' "s/\?v=1/?v=${THING}/g" docs/index.html
+echo "Incremented Version to ${THING}"
