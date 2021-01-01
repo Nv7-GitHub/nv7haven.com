3783cdfe360e422254cdf6fb65d252a510446c00
Nv7 <junk4Nv7@gmail.com>
2020-12-31 17:36:43 -0800

    Fix buildscript

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ GOOS=js GOARCH=wasm go build -ldflags="-w -s" -o docs/main.wasm
 echo "Built"
 du -h docs/main.wasm
 
-THING=$(cat docs/index.html | pcregrep -o1 '\?v=([0-9]+)')
-THING=$((THING + 1))
-sed -i '' "s/\?v=1/?v=${THING}/g" docs/index.html
-echo "Incremented Version to ${THING}"
+THING=$(cat docs/index.html | pcregrep -o1 '\?v=(\d+)')
+NUM=$((THING + 1))
+sed -i '' "s/?v=${THING}/?v=${NUM}/g" docs/index.html
+echo "Incremented Version to ${NUM}"
