c2c5f082979d7d4a5a2ef60865a2cbf93af19dd4
Nv7 <junk4Nv7@gmail.com>
2021-03-18 15:44:24 -0700

    Recompile

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 go generate
 echo "Generated"
-GOOS=js GOARCH=wasm go build -ldflags="-w -s" -o docs/main.wasm
+DEBUGPATH="$(go env GOPATH)/src"
+GOOS=js GOARCH=wasm go build -ldflags="-w -s" -gcflags=-trimpath=$DEBUGPATH -asmflags=-trimpath=$DEBUGPATH -o docs/main.wasm
 echo "Built"
 du -h docs/main.wasm
 
