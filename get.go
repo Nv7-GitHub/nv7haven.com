f73e24718a8ceeed33a39c9451e112b1b3d71d2f
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-07-04 22:10:03 -0700

    Use fetch

--- get.go
+++ get.go
@@ -1,26 +1,30 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"io"
-	"io/ioutil"
-	"net/http"
+	"time"
+
+	fetch "marwan.io/wasm-fetch"
 
 	js "github.com/vugu/vugu/js"
 )
 
 func get(url string) string {
-	res, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-	output, err := ioutil.ReadAll(res.Body)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := fetch.Fetch(url, &fetch.Opts{
+		Method: fetch.MethodGet,
+		Signal: ctx,
+	})
 	if err != nil {
 		js.Global().Call("alert", err.Error())
 		panic(err)
 	}
-	if res.StatusCode == 500 {
+	output := string(res.Body)
+	if res.Status == 500 {
 		js.Global().Call("alert", output)
 		panic(errors.New(string(output)))
 	}
@@ -28,32 +32,38 @@ func get(url string) string {
 }
 
 func getCode(url string) (string, int) {
-	res, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer res.Body.Close()
-	output, err := ioutil.ReadAll(res.Body)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := fetch.Fetch(url, &fetch.Opts{
+		Method: fetch.MethodGet,
+		Signal: ctx,
+	})
 	if err != nil {
 		js.Global().Call("alert", err.Error())
 		panic(err)
 	}
-	return string(output), res.StatusCode
+	return string(res.Body), res.Status
 }
 
 func post(url string, kind string, data io.Reader) string {
-	res, err := http.Post(url, kind, data)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := fetch.Fetch(url, &fetch.Opts{
+		Method: fetch.MethodPost,
+		Signal: ctx,
+		Body:   data,
+		Headers: map[string]string{
+			"Content-Type": kind,
+		},
+	})
 	if err != nil {
 		js.Global().Call("alert", err.Error())
 		panic(err)
 	}
-	defer res.Body.Close()
-	output, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		js.Global().Call("alert", err.Error())
-		panic(err)
-	}
-	if res.StatusCode == 500 {
+	output := string(res.Body)
+	if res.Status == 500 {
 		js.Global().Call("alert", output)
 		panic(errors.New(string(output)))
 	}
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.16
 require (
 	github.com/Nv7-Github/Bpp v0.0.0-20210611225425-6e828dc231ab
 	github.com/lucasb-eyer/go-colorful v1.2.0
-	github.com/r3labs/sse/v2 v2.3.2
 	github.com/vugu/vjson v0.0.0-20200505061711-f9cbed27d3d9
 	github.com/vugu/vugu v0.3.4
+	marwan.io/wasm-fetch v0.1.0
 )
