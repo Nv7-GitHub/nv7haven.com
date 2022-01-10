9f7c028561ed9d0503658bac742c8d7a2dec2924
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-01-09 23:17:44 -0800

    Make chess smaller

--- build_chess.sh
+++ build_chess.sh
@@ -1,3 +1,3 @@
 cd "$(go env GOPATH)/src/github.com/Nv7-Github/chess"
-GOOS=js GOARCH=wasm go build -o "$(go env GOPATH)/src/github.com/Nv7-Github/nv7haven.com/docs/chess.wasm"
+GOOS=js GOARCH=wasm go build -o "$(go env GOPATH)/src/github.com/Nv7-Github/nv7haven.com/docs/chess.wasm" -ldflags="-s -w"
 cd "$(go env GOPATH)/src/github.com/Nv7-Github/nv7haven.com"
